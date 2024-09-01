af56bccf18b237a50a22a66b0c6c285ef73f8e1f
hagemon <ooonefolder@gmail.com>
2024-09-01 15:58:59 +0800

    change icon

--- src/header/header.js
+++ src/header/header.js
@@ -1,7 +1,10 @@
 function Header() {
     return (
         <div className="flex items-center justify-between pl-4">
-            <h1 className="text-3xl font-bold">Random Vim Challenge<span className="text-3xl pl-2">🔀</span></h1>
+            <h1 className="text-3xl font-bold flex items-center">
+                <img src="/logo48.png" alt="Random Vim Challenge Logo" className="h-8 w-8 mr-2" />
+                Random Vim Challenge
+            </h1>
         </div>
     )
 }
